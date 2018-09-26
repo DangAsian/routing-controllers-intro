0851866c91712bb319763241c33c180146477665
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-26 15:53:50 -0400

    specified the data for each pages instance variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
   def welcome
     # render :contest
+    @header = "This is the welcome page for my site, please abide to my rules"
   end
 
   def about
-
+    @header = "This is the about section, but what about it stands out?"
   end
 
   def contest
-
+    @header = "Contest to rule them all... sounds familiar"
   end
 end
